d4803d78b646b29ca647768e7c20a345ba38544b
J.J.Johnson <email4johnson@gmail.com>
2016-06-21 19:42:14 -0400

    Adding PHP with hardcoded values

---
+++ mathtest.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<body>
+<h1>Math Game</h1>
+<form action="mathtest.php" method="post">
+What is 107 + 20?
+<input name="answer" value="Enter answer">
+</form>
+</body>
+</html>
