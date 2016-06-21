d4aa2358498d9b865f8c813df6d63bf8ec7bc087
J.J.Johnson <email4johnson@gmail.com>
2016-06-21 19:56:00 -0400

    Processing form
    not checking answer

--- mathtest.php
+++ mathtest.php
@@ -4,10 +4,18 @@
 <h1>Math Game</h1>
 <form action="mathtest.php" method="post">
 <?php
-$val1 = rand(100,1000);
-$val2 = rand(1,100);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $val1 = $_POST["val1"];
+  $val2 = $_POST["val2"];
+  $answer = $_POST["answer"];
+} else {
+  $val1 = rand(100,1000);
+  $val2 = rand(1,100);
+}
 ?>
 What is <?=$val1?> + <?=$val2?>?
+<input type="hidden" name="val1" value=<?=$val1?>>
+<input type="hidden" name="val2" value=<?=$val2?>>
 <input name="answer" value="Enter answer">
 </form>
 </body>
