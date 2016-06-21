7a430259db7b4b8c2584a503688ec0546491be78
J.J.Johnson <email4johnson@gmail.com>
2016-06-21 19:50:29 -0400

    Fleshing out the file.
    Replaced DEBUG with variables. fja;djja; ajf;ajd;
    fffffffffffffffffffffffffffffffffffffffffffffffffffffffffj fjlsjljljjljljjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj  lllllllllllllllllllllllllllllllllllllllllllllllllllllllllll

--- mathtest.php
+++ mathtest.php
@@ -4,9 +4,10 @@
 <h1>Math Game</h1>
 <form action="mathtest.php" method="post">
 <?php
-var_dump(rand(100,1000));
+$val1 = rand(100,1000);
+$val2 = rand(1,100);
 ?>
-What is 107 + 20?
+What is <?=$val1?> + <?=$val2?>?
 <input name="answer" value="Enter answer">
 </form>
 </body>
